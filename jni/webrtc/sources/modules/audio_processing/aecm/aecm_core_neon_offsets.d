33482a9b29660a2b115dc61e54f39acfc5b54d95
serj <github@serj.de>
2013-01-02 16:29:19 +0100

    Add webrtc static (UGLY)

---
+++ jni/webrtc/sources/modules/audio_processing/aecm/aecm_core_neon_offsets.d
@@ -0,0 +1,41 @@
+sources/modules/audio_processing/aecm/aecm_core_neon_offsets.S: \
+ sources/modules/audio_processing/aecm/aecm_core_neon_offsets.c \
+ sources/modules/audio_processing/aecm/aecm_core.h \
+ sources/modules/audio_processing/aecm/../../../typedefs.h \
+ /opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ sources/modules/audio_processing/aecm/../../../common_audio/signal_processing/include/signal_processing_library.h \
+ /opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/string.h \
+ /opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/malloc.h \
+ sources/modules/audio_processing/aecm/../../../common_audio/signal_processing/include/spl_inl.h \
+ sources/modules/audio_processing/aecm/aecm_defines.h
+
+sources/modules/audio_processing/aecm/aecm_core.h:
+
+sources/modules/audio_processing/aecm/../../../typedefs.h:
+
+/opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+sources/modules/audio_processing/aecm/../../../common_audio/signal_processing/include/signal_processing_library.h:
+
+/opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/string.h:
+
+/opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/opt/android-ndk-r8c/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+sources/modules/audio_processing/aecm/../../../common_audio/signal_processing/include/spl_inl.h:
+
+sources/modules/audio_processing/aecm/aecm_defines.h:
